8b020078bc3e90ecd922cdf4ab6ec4e772eae7dd
Andrii Kirchei <kircheiandri@gmail.com>
2023-11-13 12:05:40 +0200

    init project, add router

---
+++ src/pages/Wallet/Wallet.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Wallet.css';
+
+const Wallet = () => {
+    return (
+        <div className="Wallet">
+            Wallet
+        </div>
+    );
+}
+
+export default Wallet;
