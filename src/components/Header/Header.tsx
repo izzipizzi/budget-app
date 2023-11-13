0ef6bd6b9f942bebae38e901eef7b08f6180e6a7
Andrii Kirchei <kircheiandri@gmail.com>
2023-11-13 19:03:12 +0200

    add header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Avatar, Stack, Typography } from '@mui/material';
+import { ReactComponent as BellIcon } from '@images/svg/bell.svg';
+
+const Header = () => {
+  return (
+    <Stack
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      spacing={0}
+    >
+      <Avatar alt="USer avatar" variant="rounded"
+        src="https://gravatar.com/avatar/0b60671029d8510949c94f756ae0ccf7?s=200&d=robohash&r=g"/>
+      <Typography variant="title">
+                Home
+      </Typography>
+      <BellIcon/>
+    </Stack>
+  );
+};
+
+export default Header;
